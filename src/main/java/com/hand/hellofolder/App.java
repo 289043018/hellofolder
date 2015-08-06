485d84f0c48253b79879f3034e7bbde08513159e
huangziliang <289043018@qq.com>
2015-08-06 21:25:35 +0800

    add mkdirs

--- src/main/java/com/hand/hellofolder/App.java
+++ src/main/java/com/hand/hellofolder/App.java
@@ -6,16 +6,23 @@ public class App
 {
     public static void main( String[] args )
     {
+//        File folder = new File("my new file/one/two/three/main");
+//        if(folder.mkdirs()){
+//        	System.out.println("文件夹创建成功");
+//        }else{
+//        	if(folder.exists()){
+//        		System.out.println("文件夹已经存在");
+//        	}else{
+//        		System.out.println("文件夹创建失败");
+//        		
+//        	}
+//        }
         File folder = new File("my new file");
-        if(folder.mkdirs()){
-        	System.out.println("文件夹创建成功");
+        File newfolder = new File("my new file-new");
+        if(folder.renameTo(newfolder)){
+        	System.out.println("done");
         }else{
-        	if(folder.exists()){
-        		System.out.println("文件夹已经存在");
-        	}else{
-        		System.out.println("文件夹创建失败");
-        		
-        	}
+        	System.out.println("fail");
         }
         
     }
